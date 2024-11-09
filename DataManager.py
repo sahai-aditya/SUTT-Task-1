5b30597f7c4793dcd485ad4a83331daca4dbf840
Aditya Sahai <sahai.aditya.0204@gmail.com>
2024-11-10 03:14:47 +0530

    Storing the data in json file

--- DataManager.py
+++ DataManager.py
@@ -3,7 +3,9 @@ import json
 import pandas
 
 
-EXCEL_FILE_PATH = os.path.join(os.getcwd(), "timetable-workbook.xlsx")
+BASE_DIR = os.getcwd()
+EXCEL_FILE_PATH = os.path.join(BASE_DIR, "timetable-workbook.xlsx")
+JSON_FILE_PATH = os.path.join(BASE_DIR, "output.json")
 
 SHEETS = ["S1", "S2", "S3", "S4", "S5", "S6"]
 workbook_reader = pandas.read_excel(EXCEL_FILE_PATH, sheet_name=SHEETS, skiprows=[0, 2])
@@ -18,6 +20,8 @@ data = []
 
 for sheet_name in SHEETS:
     sheet_df = workbook_reader[sheet_name]
+
+    # renaming columns for ease of development
     sheet_df.rename(columns={
         "COURSE NO.": "code",
         "COURSE TITLE": "title",
@@ -30,11 +34,13 @@ for sheet_name in SHEETS:
         "DAYS & HOURS": "slots",
     }, inplace=True)
 
+    # df_is_notna made to check if a cell is empty
     df_is_notna = sheet_df.notnull()
     course_data = {}
     section = None
 
     for index, row in sheet_df.iterrows():
+        # entering this if means the code found the line which contains course details
         if df_is_notna.loc[index]["code"] and not course_data.get("course_code"):
             course_data["course_code"] = row["code"]
             course_data["course_title"] = row["title"]
@@ -45,7 +51,8 @@ for sheet_name in SHEETS:
             }
 
             course_data["sections"] = []
-        
+
+        # entering this if means the code found the row which contains section details
         if df_is_notna.loc[index]["section"]:
             if section:
                 course_data["sections"].append(section_data)
@@ -60,13 +67,28 @@ for sheet_name in SHEETS:
             }
 
             if row["section"][0] != "P":
-                for day in row["slots"].split()[:-1]:
-                    start_time = 7 + int(row["slots"].split()[-1])
-                    end_time = start_time + 1
-                    section_data["timing"].append({
-                        "day": day,
-                        "slot": [start_time, end_time]
-                    })
+                # this approach is used to overcome the problem in thermo slots
+                timing = []
+                slots_split = row["slots"].split()
+                slot_num = None
+
+                while slots_split != []:
+                    item = slots_split.pop()
+
+                    if item.isdigit():
+                        slot_num = int(item)
+                        start_time = 7 + slot_num
+                        end_time = start_time + 1
+
+                    else:
+                        timing.append({
+                            "day": item,
+                            "timing": [start_time, end_time]
+                        })
+
+                # order of slots gets reversed because items are obtained by popping
+                # [::-1] done so that timings are from starting of the week to the end
+                section_data["timing"] = timing[::-1]
 
             elif row["section"][0] == "P":
                 day, slot, _ = row["slots"].split()
@@ -75,10 +97,13 @@ for sheet_name in SHEETS:
                     "timing": [7 + int(slot), 9 + int(slot)]
                 })
 
+        # entering this if means the code found the line which contains only the instructor and no new course/section
         elif not df_is_notna.loc[index]["section"] and row["instructor"] not in section_data["instructors"]:
             section_data["instructors"].append(row["instructor"])
-    
+
+    course_data["sections"].append(section_data)
     data.append(course_data)
 
-
-    print(json.dumps(data, indent=4))
+# dumping the data into the file
+with open(JSON_FILE_PATH, "w") as output_f:
+    json.dump(data, output_f, indent=4)
